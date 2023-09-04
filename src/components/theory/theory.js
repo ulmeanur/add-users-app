cde3ed963f9a62d75cbcb2c31c2c0b554541e04c
Raluca Ulmeanu <raluca.ioana.ulmeanu@gmail.com>
2023-09-04 20:52:52 +0300

    adding theory updates

--- src/components/theory/theory.js
+++ src/components/theory/theory.js
@@ -3,12 +3,15 @@
 
 // =============================== 
 
+//Whenever we call a State Setting Function
+//the component function re-executes / the components are rerendered
+// ===============================
 
 //State Management in React
 // is used to update the state with every keystroke (onChange)
 // and save what the user enters in such a state variable.
 
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 // =============================== 
 // create a React component
@@ -254,6 +257,90 @@ import ReactDOM from 'react-dom';
 
 
 //=========================
+// useEffect
+
+// useEffect - runs after every component render cycle/re-render if no dependencies added
+// A component is re-render each time the State changes!
+
+//Whenever we call a State Setting Function
+//the component function re-executes
+
+//useEffect hook helps you deal with code that should be executed in response to some other action.
+//useEffect handles side effects (e.g. http requests 
+
+// or  if we listen to every keystroke and save/validate that entered data 
+// like checking and updating the form validity,in response to a keystroke 
+// in the email or a password field)
+
+// useEffect can receive two arguments--->  useEffect( ()=>{}, [] )
+
+//The first argument is a function.
+//For example here an anonymous arrow function ()=>{} .
+//This function is executed by React after every component re-evaluation/is rerendered and only if the dependencies have changed!!!
+//The second argument is an array of dependencies [] .
+
+// if we use useEffect withouth dependencies and no array the func will executes each time the component is rerendered/re-evaluated
+// if we use useEffect with an empty array [] and no dependencies the func will executes only once when the app starts
+// if we use useEffect with dependencies [a,b,c] the func will executes when dependencies change
+
+//in useEffect() dependencies:
+
+//You DON'T need to add state updating functions: 
+//React guarantees that those functions never change, 
+//hence you don't need to add them as dependencies (you could though)
+
+// You also DON'T need to add "built-in" APIs or functions like fetch(), 
+//localStorage etc (functions and features built-into the browser and 
+//hence available globally): These browser APIs / global functions are not 
+//related to the React component render cycle and they also never change
+
+// You also DON'T need to add variables or functions you might've defined 
+// OUTSIDE of your components (e.g. if you create a new helper function in 
+// a separate file): Such functions or variables also are not created inside 
+// of a component function and hence changing them won't affect your components 
+// (components won't be re-evaluated if such variables or functions change and vice-versa)
+
+//Where to use useEffect() : E.g. the Login scenario where the app restarts
+//because the user left the page and comes back
+//or simply because we reload the page and we want to show 
+//logged in state if user has logged in previously
+
+
+
+useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+
+    if (storedUserLoggedInInformation === '1') {
+    //we set the value inside the useEffect() to avoid an infinite loop
+      setIsLoggedIn(true);
+      
+    }
+  }, []);
+
+
+// !! useEffect() cleanup function
+
+// Add " return () => {} " inside useEffect() in order to run the cleanup function
+// The cleanup function will run as a cleanup process 
+// before useEffect executes this function the next time.
+// So to make it more clear this cleanup runs whenever this useEffect function runs,
+// before it runs, except for the very first time when it runs.
+
+this cleanup function will run.
+
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      console.log('Checking form validity!');
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    return () => {
+      console.log('CLEANUP');
+      clearTimeout(identifier);
+    };
+  }, [enteredEmail, enteredPassword]);
 
 
 //=========================
